560e8c398fb055900b2b1a59700ae4d7a73fa856
admin <wu.jiawei17@byd.com>
2026-03-26 19:49:22 +0800

    美化HomeView界面

---
+++ src/Main/Astra/Converters/CountToVisibilityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Astra.Converters
+{
+    public class CountToVisibilityConverter : IValueConverter
+    {
+        // Count == 0 时显示（Visible），有数据时隐藏（Collapsed）
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int count)
+                return count == 0 ? Visibility.Visible : Visibility.Collapsed;
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
---
+++ src/Main/Astra/Converters/IoValueDisplayConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Astra.Converters
+{
+    public class IoValueDisplayConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var str = value?.ToString() ?? "";
+            if (str.Equals("True", StringComparison.OrdinalIgnoreCase)) return "ON";
+            if (str.Equals("False", StringComparison.OrdinalIgnoreCase)) return "OFF";
+            return str; // 模拟量直接原样显示
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
