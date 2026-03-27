775e5a26e770b57e9cc4cf889f77fe0a9065cb33
admin <wu.jiawei17@byd.com>
2026-03-27 23:29:13 +0800

    修改ScottPlot的样式

--- src/Main/Astra/Views/HomeModules/TestItemChartWindow.xaml.cs
+++ src/Main/Astra/Views/HomeModules/TestItemChartWindow.xaml.cs
@@ -1,12 +1,49 @@
 using System.Windows;
+using Astra.ViewModels.HomeModules;
+using Astra.UI.Helpers;
+using ScottPlot;
+using ScottPlot.Plottables;
 
 namespace Astra.Views.HomeModules
 {
     public partial class TestItemChartWindow : Window
     {
+        private readonly Crosshair _actualCrosshair;
+        private readonly HorizontalLine _lowerLimitLine;
+        private readonly HorizontalLine _upperLimitLine;
+
         public TestItemChartWindow()
         {
             InitializeComponent();
+
+            ItemPlot.Plot.Axes.Bottom.Label.Text = "样本";
+            ItemPlot.Plot.Axes.Left.Label.Text = "数值";
+            ApplyItemPlotStyleToAllPlots();
+
+            Loaded += OnLoaded;
+            DataContextChanged += OnDataContextChanged;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            RenderChart();
+        }
+
+        private void OnDataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            RenderChart();
+        }
+
+        private void RenderChart()
+        {
+            if (DataContext is not TestTreeNodeItem item)
+                return;
+        }
+
+        private void ApplyItemPlotStyleToAllPlots()
+        {
+            var styleOptions = ScottPlotStyleHelper.CreateThemeStyleOptions();
+            ScottPlotStyleHelper.ApplyToPlotAndSubplots(ItemPlot.Plot, ItemPlot.Multiplot, styleOptions);
         }
     }
 }
